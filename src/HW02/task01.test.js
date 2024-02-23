a64054a71ffd1d3cf0290147065d49f66b002ad8
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-02-23 20:44:11 +0300

    Добавлен coverage report и добавлены недостающие тесты.

--- src/HW02/task01.test.js
+++ src/HW02/task01.test.js
@@ -3,7 +3,12 @@ import { max } from "./task01";
 describe("console checks", ()=>{
     const logSpy = jest.spyOn(console, 'log');
 
-    it("console should show max of two numbers", () => {
+    it("console should show first number", () => {
+        max(5, 2);
+        expect(logSpy).toHaveBeenCalledWith(5);
+    })
+    
+    it("console should show second number", () => {
         max(5, 10);
         expect(logSpy).toHaveBeenCalledWith(10);
     })
