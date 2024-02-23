71565c074f78c87e78fb48f9c39115c0e60c00e2
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-02-23 13:42:00 +0300

    ДЗ №1: Решена задача 2

---
+++ src/HW01/task02.js
@@ -0,0 +1,3 @@
+export function commonLength(str1, str2) {
+    console.log(str1.length+str2.length);
+}
---
+++ src/HW01/task02.test.js
@@ -0,0 +1,11 @@
+import { commonLength } from "./task02";
+
+describe("console checks", () => {
+    it("console contains sum of lengths str1 and str2", () => {
+        const logSpy = jest.spyOn(console, 'log');
+        
+        commonLength('Hello', ' world!');
+        
+        expect(logSpy).toHaveBeenCalledWith(12);
+    })
+})
