9879a2e461a9f0719d32086e29f7a22baed3e212
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-02-23 16:12:55 +0300

    Исправление для ДЗ №1, задание 3. Убран мок, добавлено корректное отображение на index.html.

--- src/HW01/task03.js
+++ src/HW01/task03.js
@@ -1,5 +1,4 @@
-export function sumOfDigits() {
-    const number = prompt("Введите трёхзначное число:");
+export function sumOfDigits(number) {
     let sum = `${number}`.split("").reduce((memo, x) => {return Number(memo)+Number(x)});
     console.log(sum)
 }
--- src/HW01/task03.test.js
+++ src/HW01/task03.test.js
@@ -1,23 +1,11 @@
 import { sumOfDigits } from "./task03";
 
 describe("console checks", () => {
-    const logSpy = jest.spyOn(console, 'log');
-    const promptSpy = jest.spyOn(global, 'prompt');
-
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
-
-    it("should call the input", () => {
-        sumOfDigits();
-
-        expect(promptSpy).toHaveBeenCalledWith("Введите трёхзначное число:");
-    });
     it("console contains sum of digits", () => {
-        promptSpy.mockReturnValue(123);
-
-        sumOfDigits();
-
+        const logSpy = jest.spyOn(console, 'log');
+        
+        sumOfDigits(123);
+        
         expect(logSpy).toHaveBeenCalledWith(6);
     })
 })
