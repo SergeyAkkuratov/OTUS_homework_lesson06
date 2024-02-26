57ac3127119f865eec819f3c5eeb61e44585eecd
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-02-26 20:50:36 +0300

    ДЗ 06: Решение задачи №2.

---
+++ src/HW06/task02.test.js
@@ -0,0 +1,13 @@
+import { isWord } from "./task02";
+
+describe("Task02", ()=>{
+    it("word === true", () => {
+        expect(isWord('word')).toBe(true);
+    })
+    it("Hello, word === false", () => {
+        expect(isWord('Hello, word')).toBe(false);
+    })
+    it("_1234dasc21fdswaf === true", () => {
+        expect(isWord('_1234dasc21fdswaf')).toBe(true);
+    })
+})
