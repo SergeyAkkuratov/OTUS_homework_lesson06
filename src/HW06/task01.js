f0e16fdf8bad59516bfa2a5092c5c4b7c8a09d82
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-03-05 14:52:01 +0300

    Исправил модули.

--- src/HW06/task01.js
+++ src/HW06/task01.js
@@ -1,3 +1,3 @@
 export function diff(num1, num2) {
-    return Math.abs(Math.abs(num1)-Math.abs(num2));
+    return Math.abs(num1-num2);
 }
--- src/HW06/task01.test.js
+++ src/HW06/task01.test.js
@@ -10,4 +10,15 @@ describe("Task01", () => {
     it("diff(6,6) == 0", () => {
         expect(diff(6, 6)).toBe(0);
     })
+
+    it("diff(-3,-2) == 1", () => {
+        expect(diff(-3, -2)).toBe(1);
+    })
+
+    it("diff(-3,2) == 5", () => {
+        expect(diff(-3, 2)).toBe(5);
+    })
+    it("diff(3,-2) == 5", () => {
+        expect(diff(3, -2)).toBe(5);
+    })
 })
